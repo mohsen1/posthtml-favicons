fe134aabb37b61dd55ab7a479b7b7d78f0fe5bee
Alexander Ein <435658+anein@users.noreply.github.com>
2019-06-19 20:09:14 +0300

    ref: remove unused code

--- __tests__/plugin.spec.ts
+++ __tests__/plugin.spec.ts
@@ -1,43 +1,35 @@
 import posthtml from "posthtml";
 
-import plugin from "../src";
 import { Configuration, FavIconResponse } from "favicons";
-
-jest.mock("fs");
-jest.mock("util", () => ({
-    promisify(fn: Function) {
-        return fn;
-    }
-}));
+import plugin from "../src/";
 
 jest.mock("favicons", () => (input: string, config: any, cb: Function) => {
-    setTimeout(() => {
-        const res: FavIconResponse = {
-            images: [{ name: "foo", contents: Buffer.alloc(10) }],
-            files: [{ name: "bar", contents: Buffer.alloc(10) }],
-            html: ['<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">']
-        };
-        cb(null, res);
-    }, 1);
+    const res: FavIconResponse = {
+        images: [{ name: "foo", contents: Buffer.alloc(10) }],
+        files: [{ name: "bar", contents: Buffer.alloc(10) }],
+        html: ['<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">']
+    };
+    cb(null, res);
 });
 
+//
+// Here we're checking the correctness of the replacement of a source link tag by generated link tags with icons.
+//
 describe("basics", () => {
-    let config: Partial<Configuration>;
+    let iconPath: string = "./__tests__/icon.png";
 
-    beforeEach(() => {
-        config = {
-            icons: {
-                android: false,
-                appleIcon: false,
-                appleStartup: false,
-                coast: false,
-                favicons: true,
-                firefox: false,
-                yandex: false,
-                windows: false
-            }
-        };
-    });
+    let config: Partial<Configuration> = {
+        icons: {
+            android: false,
+            appleIcon: false,
+            appleStartup: false,
+            coast: false,
+            favicons: true,
+            firefox: false,
+            yandex: false,
+            windows: false
+        }
+    };
 
     it("does nothing if a link tag does not exist", async () => {
         const html = `
@@ -53,6 +45,7 @@ describe("basics", () => {
         const result = await posthtml()
             .use(plugin({ configuration: config }))
             .process(html);
+
         expect(result.html).toMatchSnapshot();
     });
 
@@ -72,26 +65,7 @@ describe("basics", () => {
         const result = await posthtml()
             .use(plugin({ configuration: config }))
             .process(html);
+
         expect(result.html).toMatchSnapshot();
-    });
-
-    it("appends 1 basic tags", async function() {
-        const iconPath = "./__tests__/icon.png";
-        const html = `
-            <!doctype html>
-            <html>
-                <head>
-                    <link rel="icon" href="${iconPath}" />
-                </head>
-                <body>
-                </body>
-            </html>
-        `;
-
-        await posthtml()
-            .use(plugin({ outDir: "./dist/assets", configuration: config }))
-            .process(html)
-            .then((res: any) => { console.log(res.html)});
-        // expect(result.html).toMatchSnapshot();
     });
 });
